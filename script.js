03d8a4adc2a535f496cd00fbe213ab646ec8d2dd
DThomas51 <thomas.dufour974@gmail.com>
2021-03-18 17:02:20 +0100

    Responsive iphone ipad desktop Finish

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const cards = document.querySelector(".cards");
 const presentation = [
   {
     name: "Bastien",
-    picture: "./img/bastien.jpg"
+    picture: "./img/bastien.jpg",
+    text : "Ce que j’ai le plus aimé dès mon arrivée à l’école, c’est la diversité des profils et le cadre offert par la Wild Code School. Apprendre (à apprendre) dans cet environnement est vraiment super enrichissant!\n La cohésion entre tous les élèves est superbe, nous échangeons et nous nous aidons tous énormément ce qui permet d’avancer ensemble.\n Je sais que cette formation est un réel tremplin pour mon avenir professionnel et j’ai déjà hâte d’être en haut de la rampe pour prendre mon envol !" 
+    
   },
+  
   {
     name: "Thomas",
-    picture: "./img/Thomas.jpg"
-  },]
-  
+    picture: "./img/Thomas.jpg",
+    text : "La Wild c'est quoi pour moi?\nC'est avant tout une grand famille qui regroupe plusieurs écoles en Europe.\nLeurs Formations de developpeur sont les meilleures qu'on puisse trouver.\nOn est super bien suivis par les formateurs.\n'Fin bref c'est les meilleurs."
+  },
+]
 
-function createCard(title, image) {
+function createCard(title, image, text) {
   const card = document.createElement("div");
   card.classList.add("card");
   cards.appendChild(card);
@@ -35,16 +39,17 @@ function createCard(title, image) {
   cardTitle.innerHTML = `${title}`;
   cardBody.appendChild(cardTitle);
 
-  const cardButton = document.createElement("button");
-  cardButton.classList.add("card-button");
-  cardButton.innerHTML = "Témoignage";
-  cardBody.appendChild(cardButton);
+  const cardText = document.createElement("p");
+  cardText.classList.add("card-text");
+  cardText.innerHTML = `${text}`;
+  cardBody.appendChild(cardText);
+
+
 }
 
- let i = 0;
+let i = 0;
 
 for (i in presentation) {
-  createCard(presentation[i].name, presentation[i].picture);
+  createCard(presentation[i].name, presentation[i].picture, presentation[i].text);
 }
 
-console.log(cards);
